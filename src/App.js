5cc5dab3e2bab71d847cb743154ab6343271c805
Kit Chow <chowkitying@outlook.com>
2022-08-02 01:59:32 +0800

    Set up Routes & Home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { useAuthState } from "./contexts/AuthContext";
+import Navbar from "./components/Navbar";
+import homeSvg from "./assets/home.svg";
 
 const App = () => {
-  return <div>GAStack Template Locked and Loaded</div>;
+  const { name } = useAuthState().user;
+  return (
+    <>
+      <Navbar />
+      <div
+        className="container-xl d-flex flex-column justify-content-center align-items-center"
+        style={{ marginTop: "8em" }}
+      >
+        <div className="" style={{ maxWidth: "550px" }}>
+          <img src={homeSvg} className="img-fluid" alt="welcomesvg" />
+        </div>
+        <h1 className="display-4 mt-4">Hi {name}</h1>
+      </div>
+    </>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { AuthProvider } from "./contexts/AuthContext";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import SharedLayout from "./pages/SharedLayout";
+import Home from "./pages/Home";
+import SignUpForm from "./pages/SignUp";
+import LogInForm from "./pages/LogIn";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/" element={<SharedLayout />}>
+            <Route path="home" element={<Home />} />
+            <Route path="login" element={<LogInForm />} />
+            <Route path="signup" element={<SignUpForm />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
 );
