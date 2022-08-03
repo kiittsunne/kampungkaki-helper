52825d761bb42b6958cac13acf5e1cd3d004c95c
lesterzeng <lesterzengsm@gmail.com>
2022-08-03 15:59:34 +0800

    okay

--- backend/router/UserRouter.js
+++ backend/router/UserRouter.js
@@ -9,6 +9,8 @@ const auth = require("../middlewares/auth");
 
 // Import model
 const User = require("../models/User");
+// Import model
+const Job = require("../models/Job");
 
 // ====================== Routes ====================== //
 
@@ -56,6 +58,53 @@ router.put("/signup", async (req, res) => {
     res.status(201).send({ status: 201, message: "User created" });
   } catch (err) {
     res.status(500).send({ status: 500, message: err.message });
+  }
+});
+
+//////////// create job
+router.put("/createjob", async (req, res) => {
+  try {
+    // store to db
+    const createdJob = new Job({
+      title: req.body.title,
+      dateTime: req.body.dateTime,
+      neighbourhood: req.body.neighbourhood,
+      kakiName: req.body.kakiName,
+      kakiAddress: req.body.kakiAddress,
+      kakiPhone: req.body.kakiPhone,
+      description: req.body.description,
+      priority: req.body.priority,
+      pax: req.body.pax,
+      specialRequirements: req.body.specialRequirements,
+      volunteer: req.body.volunteer,
+      duration: req.body.duration,
+    });
+    await createdJob.save();
+    res.status(201).send({ status: 201, message: "User created" });
+  } catch (err) {
+    res.status(500).send({ status: 500, message: err.message });
+  }
+});
+
+///////////// display a job
+router.post("/jobs/:id", async (req, res) => {
+  try {
+    const singleJob = await Job.findOne({ _id: req.params.id });
+    res.json(singleJob);
+  } catch (error) {
+    console.log("POST/ jobs", error);
+    res.status(400).json({ status: "error", message: "an error has occured" });
+  }
+});
+
+///////////// display all job
+router.post("/jobs", async (req, res) => {
+  try {
+    const allJobs = await Job.find();
+    res.json(allJobs);
+  } catch (error) {
+    console.log("POST/ jobs", error);
+    res.status(400).json({ status: "error", message: "an error has occured" });
   }
 });
 
--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,6 @@ connectDB();
 const UserRouter = require("./router/UserRouter");
 app.use("/api", UserRouter);
 
-const JobRouter = require("./router/JobRouter");
-app.use("/admin", JobRouter);
-
 app.listen(PORT, () => {
   console.log(`App is tuned in to ${PORT}`);
 });
