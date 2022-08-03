88233e8169f7dcc75cc29459b449196c3e36139f
lesterzeng <lesterzengsm@gmail.com>
2022-08-03 14:28:16 +0800

    jobs and user schemas done

---
+++ backend/models/Job.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const JobsSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    dateTime: { type: String, required: true },
+    neighbourhood: { type: String, required: true },
+    kakiName: { type: String, required: true },
+    kakiAddress: { type: String, required: true },
+    kakiPhone: { type: Number, required: true },
+    description: { type: String, required: true },
+    priority: { type: Boolean, required: true },
+    pax: { type: Number, required: true },
+    specialRequirements: { type: String, required: true },
+    volunteer: { type: Array, required: true },
+    duration: { type: Number, required: true },
+  },
+  { collection: "Users" }
+);
+
+const Users = mongoose.model("Users", JobsSchema);
+module.exports = Users;
--- backend/models/User.js
+++ backend/models/User.js
@@ -2,9 +2,14 @@ const mongoose = require("mongoose");
 
 const UsersSchema = new mongoose.Schema(
   {
-    username: { type: String },
-    hash: { type: String },
-    email: { type: String, unique: true },
+    email: { type: String, unique: true, required: true },
+    hash: { type: String, required: true },
+    admin: { type: Boolean, required: true },
+    optIn: { type: Boolean, required: true }, // for emergency jobs
+    phoneNumber: { type: Number, required: true },
+    neighbourhood: { type: String, required: true },
+    skills: { type: Array, required: true },
+    resources: { type: Array, required: true },
   },
   { collection: "Users" }
 );
